4b0228b4fec08ef84f89f6c2c94da00408ecfc73
Abel Moreno <moreno.abel@gmail.com>
2017-08-27 16:36:02 -0500

    Solucion Module.php

--- module/Tawa/Module.php
+++ module/Tawa/Module.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Twayuu;
+namespace Tawa;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
